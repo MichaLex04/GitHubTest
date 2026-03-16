a8b5ba1a6bf0b4a16c485e8e8659d3198d5d005a
MichaLex04 <sen4lex@gmail.com>
2026-03-16 11:08:41 -0500

    asd
    asd

--- Calculadora.py
+++ Calculadora.py
@@ -1,4 +1,5 @@
 #MODULO SUMA-RESTA POR JHOSEPH COMO COLABORADOR
+print("=== Operaciones Matemáticas ===")
 try:
     numero1 = float(input("Ingresa un numero"))
     numero2 = float(input("Ingresa otro numero"))
@@ -13,7 +14,20 @@ except ValueError:
     print("Error")
 
 #MODULO SUMA-RESTA POR JASON COMO COLABORADOR
+# Ingreso de datos
+num1 = float(input(": 300"))
+num2 = float(input("200: "))
 
+# Proceso
+multiplicacion = num1 * num2
 
+if num2 != 0:
+    division = num1 / num2
+else:
+    division = "No se puede dividir entre cero"
+
+# Salida de resultados
+print("Resultado de la multiplicación:", multiplicacion)
+print("Resultado de la división:", division)
 
 #DUEÑO DEL REPOSITORIO MLX
