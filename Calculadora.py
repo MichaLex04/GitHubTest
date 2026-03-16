d26943cc76332d26a98338afdc6a07f751561cad
MichaLex04 <sen4lex@gmail.com>
2026-03-16 11:03:11 -0500

    asd
    asd

--- Calculadora.py
+++ Calculadora.py
@@ -1,3 +1,4 @@
+#MODULO SUMA-RESTA POR JHOSEPH COMO COLABORADOR
 try:
     numero1 = float(input("Ingresa un numero"))
     numero2 = float(input("Ingresa otro numero"))
@@ -10,7 +11,9 @@ try:
         print(f"El resultado de {numero1} - {numero2} es: {resultado}")
 except ValueError:
     print("Error")
-"""
-JS Multiplicacion
-JS Division
-"""
+
+#MODULO SUMA-RESTA POR JASON COMO COLABORADOR
+
+
+
+#DUEÑO DEL REPOSITORIO MLX
